5c5e9a457543d6b9fd025e0e3850304606ade76d
The-pixel04 <krsyrdyrd@gmail.com>
2025-03-10 20:45:08 +0200

    Create functionality

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,17 +1,9 @@
-const baseUrl= 'http://localhost:3030/jsonstore/games';
+import requester from "../../utils/requester.js";
 
-export default{
-    async create(gameData){
-        const response = await fetch(baseUrl, {
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
-            },
-            body: JSON.stringify(gameData);
-        });
+const baseUrl = 'http://localhost:3030/jsonstore/games';
 
-        const result=await response.json();
-
-        return result;
+export default {
+    create(gameData) {
+        return requester.post(baseUrl, gameData);
     }
 }
---
+++ client/utils/requester.js
@@ -0,0 +1,6 @@
+export default {
+    get: request.bind(null, 'GET'),
+    post: request.bind(null, 'POST'),
+    put: request.bind(null, 'PUT'),
+    delete: request.bind(null, 'DELETE')
+}
