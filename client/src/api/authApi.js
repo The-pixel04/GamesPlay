c4047210747d1204b8e2eb8141a5b5d89a453247
The-pixel04 <krsyrdyrd@gmail.com>
2025-03-17 20:26:37 +0200

    Register functionality

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -4,7 +4,7 @@ import requester from "../utils/requester.js"
 const baseUrl = 'http://localhost:3030/users'
 
 export const useLogin = () => {
-    const abortRef  = useRef(new AbortController())
+    const abortRef = useRef(new AbortController());
 
     const login = async (email, password) => {
         const result = await requester.post(`${baseUrl}/login`, { email, password }, { signal: abortRef.current.signal });
@@ -14,11 +14,21 @@ export const useLogin = () => {
 
     useEffect(() => {
         const abortController = abortRef.current;
-        
+
         return () => abortController.abort();
-    },[]);
+    }, []);
 
     return {
         login
     }
-}
+};
+
+export const useRegister = () => {
+    const register = (email, password) => {
+        return requester.post(`${baseUrl}/register`, { email, password });
+    }
+
+    return {
+        register
+    }
+};
